fa5fa6cf4819514137ec748dfc8900dad9da89f8
Rohan <rohan.vimal@iphtechnologies.com>
2025-03-24 11:32:03 +0530

    project-main-files
    main files of the project.

---
+++ LoginCard/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Provider } from "react-redux";
+
+import { ToastContainer } from 'react-toastify';
+import store, { persistor } from "./Redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     
+
+      <Provider store={store}>
+       
+       
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+            <ToastContainer />
+        </PersistGate>
+      </Provider>
+    
+   
+
+  </StrictMode>,
+)
